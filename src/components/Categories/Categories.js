6d3ac813710a87d3c6a99111701dd5c6a4ab3c5d
IlyaSid231 <144792946+IlyaSid231@users.noreply.github.com>
2025-10-13 10:45:34 +0300

    feat: add categories component

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import fruitJuice from "../../img/fruit_juice.png"
+import nutsCookies from "../../img/nuts-cookies.png"
+import organicFood from "../../img/organic_food.png"
+import './Categories.css'
+
+const Categories = () => {
+  return (
+      <div className="eighth-block">
+        <div class="eighth-block-fruit-juice" style={{backgroundImage: `url(${fruitJuice})`}}>
+                <p>Fruit Juice</p>
+            </div>
+            <div class="eighth-block-organic-food" style={{backgroundImage: `url(${nutsCookies})`}}>
+                <p>Organic Food</p>
+            </div>
+            <div class="eighth-block-nuts-cookies" style={{backgroundImage: `url(${organicFood})`}}>
+                <p>Nuts Cookies</p>
+            </div>
+      </div>
+    );
+};
+
+export default Categories;
