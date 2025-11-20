211fe715f6d22ebaa08fc14643592affb4457332
Ilya LIFE <himia744@gmail.com>
2025-11-20 21:08:09 +0300

    feat: add constraints

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,13 +5,13 @@ const Orders = require('./Orders');
 const OrderProducts = require('./OrderProducts');
 
 // Ассоциации
-Counterparties.hasMany(Orders, { foreignKey: 'counterparty_id' });
+Counterparties.hasMany(Orders, { foreignKey: 'counterparty_id', onDelete: 'CASCADE', });
 Orders.belongsTo(Counterparties, { foreignKey: 'counterparty_id' });
 
-Orders.hasMany(OrderProducts, { foreignKey: 'order_id' });
+Orders.hasMany(OrderProducts, { foreignKey: 'order_id', onDelete: 'CASCADE', });
 OrderProducts.belongsTo(Orders, { foreignKey: 'order_id' });
 
-Products.hasMany(OrderProducts, { foreignKey: 'product_id' });
+Products.hasMany(OrderProducts, { foreignKey: 'product_id', onDelete: 'CASCADE', });
 OrderProducts.belongsTo(Products, { foreignKey: 'product_id' });
 
 module.exports = {
